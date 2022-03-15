6bc0ddb241755af84b3ef1382b343a5ec87d2f0b
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2022-03-15 21:42:38 +0530

    q1 done

---
+++ module 3-Node js/questions/q2.js
@@ -0,0 +1,6 @@
+let fs=require("fs");
+let path=require("path");
+
+
+// given an array arr=[audio,video,image,software,documents,applications,other]
+//make folder for each element in the given array and inside each folder make 4 files of that type
